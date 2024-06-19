451f1b24d2b269a77ab58ad08a9e0cad787be9d2
zhangou <zhangou@wepie.com>
2024-06-19 17:10:09 +0800

    uid 设置

--- app/store/mysql.go
+++ app/store/mysql.go
@@ -117,8 +117,8 @@ func (sc *SqlClient) SaveUser(user *User) (int64, error) {
 	if user == nil {
 		return 0, errors.New("param empty")
 	}
-	query := "UPDATE user SET name = ?, vipTime = ?, version = ?, password = ?, data = ?, clientIp = ?, createTime = ?, device = ?"
-	ret, err := sc.c.Exec(query, user.Name, user.VipTime, user.Version, user.Password, user.Data, user.ClientIP, user.CreateTime, user.Device)
+	query := "UPDATE user SET name = ?, vipTime = ?, version = ?, password = ?, data = ?, clientIp = ?, createTime = ?, device = ? where uid = ?"
+	ret, err := sc.c.Exec(query, user.Name, user.VipTime, user.Version, user.Password, user.Data, user.ClientIP, user.CreateTime, user.Device, user.Uid)
 	if err != nil {
 		return 0, err
 	}
